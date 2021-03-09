a9e74674c7af69735196006b39a0a957d065cf03
Ricardo Augusto de Sousa Garcia da Silva <ricardoaugustogarcia@hotmail.com>
2021-03-09 08:09:56 -0300

    Adjusting details for deploy

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 
-const Header = (props) => <Nav>Clc</Nav>;
+const Header = (props) => <Nav>Overtime Calculator</Nav>;
 
 const Nav = styled.nav`
   display: flex;
@@ -13,6 +13,13 @@ const Nav = styled.nav`
   justify-content: space-between;
   align-items: center;
   height: 60px;
+  font-size: 1.95em;
+  padding: 10px 30px;
+  text-transform: uppercase;
+  text-align: center;
+  font-family: "Bitter", serif;
+  text-decoration: none;
+  color: #fff;
   a {
     margin: 0;
     font-size: 1.95em;
